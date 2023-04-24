3df25329a40df88ca7f6336b5babda65fb133523
Yakir Hasid <hasidyakir@gmail.com>
2023-04-24 11:51:48 +0300

    we are amazing and great coders
    Co-authored-by: AlmogRabani <AlmogRabani@users.noreply.github.com>

---
+++ topnav.js
@@ -0,0 +1,45 @@
+let HOME_NAME = "Home"
+let NEWS_NAME = "News"
+let CONTACT_NAME = "Contact"
+let ABOUT_NAME = "About"
+
+// arr that contains names of pages in the website
+// that each page will be added to the topnav
+// with inner text of the web page itself
+// and link to the website itself
+let pageNameArr = ["Home", "News", "Contact", "About"]
+
+function topnavLoader(curPage) {
+
+    let topnav = document.createElement("div");
+    topnav.setAttribute("class", "topnav");
+    
+    for(let name of pageNameArr) {
+        // create element
+        let x = document.createElement("a");
+
+        //set id
+        x.setAttribute("id", name)
+
+        // set inner text of all childs
+        x.innerText = name;
+
+        // set href link
+        x.setAttribute("href", name + ".html");
+
+        // decide which nav is active
+        if(name==curPage){ x.classList.add("active")}
+
+        //add element to topnav
+        topnav.appendChild(x);
+    }
+
+    // add topnav to body
+    document.body.appendChild(topnav);
+
+    let h1 = document.createElement("h1");
+    h1.innerText = "Hello this is " + curPage;
+    document.body.appendChild(h1);
+
+ 
+}
