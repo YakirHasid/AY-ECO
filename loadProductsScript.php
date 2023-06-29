38c319ad2c82b52fa745cfb4e37d6c839025f25e
YakirHasid <hasidyakir@gmail.com>
2023-06-29 15:00:09 +0300

    php code to return all products

---
+++ loadProductsScript.php
@@ -0,0 +1,30 @@
+<?php    
+    $servername = "sql309.byethost17.com";
+    $username = "b17_33864062";
+    $password = "kTs6nN8a.Pvhi!X";
+    $dbname = "b17_33864062_AY_ECO_Database";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if($conn->connect_error) {
+        die("Connection failed: ". $conn->connect_error);
+    }
+
+    $sql = "SELECT ImageURL,Name,Description,Price FROM Product";
+
+    $answer = $conn->query($sql);               
+
+    if($conn->query($sql)) {
+
+        $result = array();
+
+        while($row = $answer->fetch_assoc()) {
+            array_push($result, $row);           
+        }        
+        
+        echo json_encode($result);          
+    }
+    else {
+        echo "Error: " .$sql . "<br>" . $conn->error;        
+    }    
+?>
