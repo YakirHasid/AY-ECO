0baf85225a799dbe2f4310e4df7a57abd588e5bb
YakirHasid <hasidyakir@gmail.com>
2023-07-16 16:10:06 +0300

    script that returns the number of posts by user

---
+++ NumOfPostsByUserScript.php
@@ -0,0 +1,27 @@
+<?php
+    $servername = "sql309.byethost17.com";
+    $username = "b17_33864062";
+    $password = "kTs6nN8a.Pvhi!X";
+    $dbname = "b17_33864062_AY_ECO_Database";
+    
+    $usernameWeb = $_GET["username"]; 
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if($conn->connect_error) {
+        die("Connection failed: ". $conn->connect_error);
+    }
+
+    $sql = "SELECT COUNT(id) FROM Post WHERE username = '".$usernameWeb."';";      
+    
+    $answer = $conn->query($sql);        
+
+    if($answer) {
+        echo json_encode($answer->fetch_assoc());
+    }
+    else {
+        echo "0";
+    }            
+    
+    $conn->close();
+?>
